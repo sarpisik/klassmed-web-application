c2af0addcb305bd2d2072f205b71c295cb72331a
sarpisik <contact@sarpisik.com>
2019-09-21 13:22:52 +0300

    Edit product layout

--- web/src/components/product.js
+++ web/src/components/product.js
@@ -5,12 +5,16 @@ import { MDBContainer, MDBRow, MDBCol } from 'mdbreact'
 
 function Product ({ title, _rawBody, categories, mainImage, publishedAt, relatedProducts }) {
   return (
-    <MDBContainer>
-      <MDBRow className='justify-content-center'>
-        <MDBCol sm={8}>
+    <MDBContainer fluid>
+      <MDBRow className='landing-background-cover text-white justify-content-center'>
+        <MDBCol>
           <h1 className='text-center'>{title}</h1>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className='justify-content-center py-5'>
+        <MDBCol sm={12} md={8} lg={6} xl={4}>
           <Img
-            style={{ height: '500px' }}
+            style={{ maxHeight: '500px' }}
             imgStyle={{
               maxHeight: '100%',
               maxWidth: '100%',
@@ -23,8 +27,12 @@ function Product ({ title, _rawBody, categories, mainImage, publishedAt, related
             alt={mainImage.alt}
             className='mx-auto d-block'
           />
-          {_rawBody && <BlockContent blocks={_rawBody || []} />}
         </MDBCol>
+        {_rawBody && (
+          <MDBCol sm={12} md={8} lg={6} xl={4} className='flex align-items-center'>
+            <BlockContent blocks={_rawBody || []} />
+          </MDBCol>
+        )}
       </MDBRow>
     </MDBContainer>
   )
